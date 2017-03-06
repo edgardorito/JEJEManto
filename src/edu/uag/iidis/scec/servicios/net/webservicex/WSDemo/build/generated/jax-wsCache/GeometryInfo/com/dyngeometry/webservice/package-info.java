ba821d7c2ff6e03268ffe5a278aa408e4b093ff6
Javier Flores <javierfh1404@gmail.com>
2017-03-05 21:53:55 -0600

    web service class

--- src/edu/uag/iidis/scec/servicios/net/webservicex/WSDemo/build/generated/jax-wsCache/GeometryInfo/com/dyngeometry/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.DynGeometry.com/WebService/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.dyngeometry.webservice;
---
+++ src/edu/uag/iidis/scec/servicios/net/webservicex/webserviceZIP/WSDemo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import com.dyngeometry.webservice.AddressInfo;
+/**
+ *
+ * @author Ernesto
+ */
+public class WSDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("country: "+geocodePro("DLXLYVSPNVZYPSNV2249", "", "", "", "33134").getGeoCity());
+    }
+
+    private static AddressInfo geocodePro(java.lang.String key, java.lang.String street, java.lang.String city, java.lang.String state, java.lang.String zip) {
+        com.dyngeometry.webservice.GeometryInfo service = new com.dyngeometry.webservice.GeometryInfo();
+        com.dyngeometry.webservice.GeometryInfoSoap port = service.getGeometryInfoSoap();
+        return port.geocodePro(key, street, city, state, zip);
+    }
+
+    
+    
+}
